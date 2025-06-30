55da5bd786c5a91d2f18f7fa9b692cae52296595
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-06-30 14:23:39 +0300

    Delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -57,4 +57,16 @@ public class StockController : ControllerBase
         _context.SaveChanges();
         return Ok(stock.ToStockDto());
     }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+        if (stock == null) return NotFound();
+        
+        _context.Stocks.Remove(stock);
+        _context.SaveChanges();
+        return NoContent();
+    }
 }
