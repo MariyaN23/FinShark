bbea7e115111d1f0cc7bdc7734221058faff7093
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-07-02 16:47:48 +0300

    Install Identity

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
+// DbContext was early
 {
     public ApplicationDbContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
