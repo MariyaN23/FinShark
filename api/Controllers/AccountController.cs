39148f286977b8292245065e527695ce2620552a
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-07-03 12:06:26 +0300

    Login

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
@@ -12,11 +13,14 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly ITokenService _tokenService;
+    private readonly SignInManager<AppUser> _signInManager;
 
-    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService,
+        SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
         _tokenService = tokenService;
+        _signInManager = signInManager;
     }
 
     [HttpPost("register")]
@@ -29,7 +33,7 @@ public class AccountController : ControllerBase
             var appUser = new AppUser
             {
                 Email = registerDto.Email,
-                UserName = registerDto.Email,
+                UserName = registerDto.Username,
             };
 
             var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
@@ -55,5 +59,22 @@ public class AccountController : ControllerBase
         {
             return StatusCode(500, ex);
         }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto loginDto)
+    {
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+        if (user == null) return Unauthorized("Invalid username");
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+        if (!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+        return Ok(
+            new NewUserDto
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            });
     }
 }
